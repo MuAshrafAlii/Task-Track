586a44da6e9c9123877601a7b4265cb8aff0de7c
Muhammad Ashraf <15Muhammad.Ashraf@gmail.com>
2025-07-24 14:06:31 +0300

    Loosly coupled using spring with defining default implementation for the interface

--- src/main/java/com/ashraf/spring1/game/MarioGame.java
+++ src/main/java/com/ashraf/spring1/game/MarioGame.java
@@ -1,8 +1,10 @@
 package com.ashraf.spring1.game;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
-
+@Component
+@Primary
 public class MarioGame implements  GamingConsole {
 
     @Override
