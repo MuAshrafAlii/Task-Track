183164e72ec80755a6491709c48c087c1ce804a8
Muhammad Ashraf <15Muhammad.Ashraf@gmail.com>
2025-08-18 07:47:25 +0300

    Simple Authentication

---
+++ src/main/java/com/muashrafalii/webapp/login/LoginController.java
@@ -0,0 +1,38 @@
+package com.muashrafalii.webapp.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+
+    private AuthenticationService authenticationService;
+
+    public LoginController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @RequestMapping(value="login", method = RequestMethod.GET)
+    public String goToLoginPage() {
+        return "login";
+    }
+
+    @RequestMapping(value="login", method = RequestMethod.POST)
+    public String goToWelcomePage(
+            @RequestParam String name,
+            @RequestParam String password, ModelMap model) {
+        model.put("name", name);
+        model.put("password", password);
+
+        //Authentication
+        // Name -> mido, Password -> dummy
+        if(authenticationService.authenticate(name,password)) {
+            return "welcome";
+        }
+        model.addAttribute("error_msg", "Invalid Credentials, Please try again");
+        return "login";
+    }
+}
---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title> Welcome Page </title>
+</head>
+
+<body>
+<h1> Hiii ${name} and welcome. We are happy to have you. Your Password is ${password} </h2>
+
+</body>
+</html>
