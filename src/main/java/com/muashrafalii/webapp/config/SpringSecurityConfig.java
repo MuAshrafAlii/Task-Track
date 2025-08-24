461b2c44e4e9e5a5289768410974e191366fb9c8
Muhammad Ashraf <15Muhammad.Ashraf@gmail.com>
2025-08-24 07:42:45 +0300

    Security Filters

--- src/main/java/com/muashrafalii/webapp/config/SpringSecurityConfig.java
+++ src/main/java/com/muashrafalii/webapp/config/SpringSecurityConfig.java
@@ -2,11 +2,14 @@ package com.muashrafalii.webapp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfig {
@@ -28,5 +31,14 @@ public class SpringSecurityConfig {
        return User.builder()
                 .passwordEncoder(password-> passwordEncoder().encode(password))
                 .username(username).password(userPassword).roles("USER", "ADMIN").build();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+        http.formLogin(Customizer.withDefaults());
+        http.csrf(csrf -> csrf.disable());
+        http.headers(h -> h.frameOptions(f -> f.disable()));
+        return http.build();
     }
 }
